ef0f76a155ca8b3777113de9b777f61aea25c33e
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-07-03 14:22:09 +0100

    basic set-up complete

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from "react";
 import './App.css';
 import {Route, Routes} from "react-router-dom";
 import {Box} from "@mui/material";
-import { Navbar, Footer } from "./components";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import ExerciseDetail from "./pages/ExerciseDetail";
 import Home from "./pages/Home";
 
 function App() {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: {xl: "1488px"} }} m="auto">
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes >
+      <Footer/>
     </Box>
   );
 }
