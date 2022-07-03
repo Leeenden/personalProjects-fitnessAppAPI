ebdc134f4f94d859215c564d80cd2408e32365f6
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-07-03 18:56:00 +0100

    added filter render on screen

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,9 +1,31 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import Pagination from "@mui/material/Pagination";
+import {Box , Stack, Typography} from "@mui/material";
 
+import { exerciseOptions, fetchData} from "../utils/fetchData";
 
-const Exercises = () => {
+const Exercises = ({exercises, setExercises, bodyPart}) => {
+  console.log(exercises);
   return (
-    <div>Exercises</div>
+    <Box id="exercises" sx={{
+      mt: {lg: "110px"}}}
+      mt="50px"
+      p="20px">
+        <Typography variant="h4">
+          Showing Results
+        </Typography>
+        <Stack 
+          direction="row"
+          sx={{
+            gap: {
+              lg: "110px",
+              xs: "50px"}}}
+          flexWrap="wrap" justifyContent="center">
+            {exercises.map((exercise, index) => (
+              <p>{exercise.name}</p>
+            ))}
+        </Stack>
+    </Box>
   )
 }
 
