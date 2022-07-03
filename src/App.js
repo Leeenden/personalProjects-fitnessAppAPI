22f3dae40d36139773c45d15661b096e2963c3e6
Linden Hamer <linden.hamer.97@hotmail.co.uk>
2022-07-03 13:32:18 +0100

    added basic components and set-up basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from "react";
 import './App.css';
+import {Route, Routes} from "react-router-dom";
+import {Box} from "@mui/material";
+import { Navbar, Footer } from "./components";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Box width="400px">
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes >
+    </Box>
   );
 }
 
